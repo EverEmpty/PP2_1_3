d3d530d9f33ba066da05e8cbe46efd0aa4467eb2
EverEmpty <fairel95@gmail.com>
2023-06-04 21:07:34 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение пары HelloWorld : " + bean.equals(bean1));
+        System.out.println("Сравнение пары Cat : " + beanCat.equals(beanCat1));
+    }
+}
